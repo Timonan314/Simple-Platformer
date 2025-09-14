b26ae9f3e170a067cc457a7dbf469579e8b3e0a4
Tim Nolan <timn783@gmail.com>
2025-09-13 20:47:29 -0400

    I added to the tileset and redid the base starting level. I also made the player animations work better.

---
+++ character_body_2d.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var facing = "right"
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += 600 * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+	
+	
+	if velocity.x > 50:
+		facing = "right"
+	if velocity.x < -50:
+		facing = "left"
+	if velocity.x > 50 and is_on_floor():
+		facing = "right"
+		animated_sprite_2d.play("right walk")
+	if velocity.x < -50 and is_on_floor():
+		facing = "left"
+		animated_sprite_2d.play("left walk")
+	if not is_on_floor():
+		if facing == "right":
+			animated_sprite_2d.play("right jump")
+		if facing == "left":
+			animated_sprite_2d.play("left jump")
+	if is_on_floor() and velocity.x == 0:
+		if facing == "right":
+			animated_sprite_2d.play("right idle")
+		if facing == "left":
+			animated_sprite_2d.play("left idle")
