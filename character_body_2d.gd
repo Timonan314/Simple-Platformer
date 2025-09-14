a2ff6a37c82b03e4c2a27d76985a51952d2fd94f
Tim Nolan <timn783@gmail.com>
2025-09-14 12:38:39 -0400

    I made the lava actually kill you if you fall into it, and also added a dash mechanic Next I need to redo the player character and also do a ton of level design.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-var facing = "right"
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
-
+var facing = 1
+@export var SPEED = 100.0
+@export var jump_vel = -200.0
+@export var dash_vel = 200
+@onready var dash_timer: Timer = $"dash timer"
+var gravity = 600
+var dash_add = 0
+var move_add = 0
 var jump_mem = 0
+var dashed = 0
 
 func _physics_process(delta: float) -> void:
 	if Global.dead == true:
@@ -12,12 +17,11 @@ func _physics_process(delta: float) -> void:
 		Global.dead = false
 		
 	##jump memory
-	print(jump_mem)
 	if not is_on_floor() and Input.is_action_just_pressed("jump"):
 		jump_mem = 6
 
 	if jump_mem > 0 and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_vel
 		jump_mem = 0
 	
 	if jump_mem > 0:
@@ -25,43 +29,62 @@ func _physics_process(delta: float) -> void:
 
 
 
+	if Input.is_action_just_pressed("dash") and dashed == 0:
+		dashed = 1
+		velocity.y = 0
+		dash_add = facing*dash_vel
+		dash_timer.start()
+		gravity = 0
+	if is_on_floor():
+		dashed = 0
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += 600 * delta
+		velocity.y += gravity * delta
+		if gravity == 0:
+			dashed = 1
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_vel
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		if move_add == 0:
+			move_add = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_add = move_toward(move_add, 0, SPEED)
+		
+	velocity.x = dash_add + move_add
+		
 	move_and_slide()
 	
 	
 	
 	if velocity.x > 50:
-		facing = "right"
+		facing = 1
 	if velocity.x < -50:
-		facing = "left"
+		facing = -1
 	if velocity.x > 50 and is_on_floor():
-		facing = "right"
+		facing = 1
 		animated_sprite_2d.play("right walk")
 	if velocity.x < -50 and is_on_floor():
-		facing = "left"
+		facing = -1
 		animated_sprite_2d.play("left walk")
 	if not is_on_floor():
-		if facing == "right":
+		if facing == 1:
 			animated_sprite_2d.play("right jump")
-		if facing == "left":
+		if facing == -1:
 			animated_sprite_2d.play("left jump")
 	if is_on_floor() and velocity.x == 0:
-		if facing == "right":
+		if facing == 1:
 			animated_sprite_2d.play("right idle")
-		if facing == "left":
+		if facing == -1:
 			animated_sprite_2d.play("left idle")
 
 
 func _on_deathbox_body_entered(body: Node2D) -> void:
 	Global.dead = true
+
+
+func _on_dash_timer_timeout() -> void:
+	dash_add = 0
+	gravity = 600
