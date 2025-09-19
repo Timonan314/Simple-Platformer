4d4132351c69ec2e589b0196202d1c767b320258
Tim Nolan <timn783@gmail.com>
2025-09-19 11:46:26 -0400

    I refined the art a bit and made the spikes actually kill you.

--- flag.gd
+++ flag.gd
@@ -12,7 +12,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	print(ID, "  ", state)
 	if not(spawn[ID] == Global.checkpoint):
 		state = "down"
 	
